2718a8a2571f3b70e1ae0b6bc84dad9d6b03f49e
MeruLin <merulylin@outlook.com>
2017-06-05 12:55:37 +0800

    springmvc+mybatis+mybatis-generator配置 person表建库脚本 generator生成实体类、实体类接口及映射文件 spring-servlet测试

---
+++ sql/create_table_person.sql
@@ -0,0 +1,8 @@
+create table person(
+id varchar(20) NOT NULL,
+password varchar(30) NOT NULL,
+name varchar(20) NOT NULL,
+role varchar(10) NOT NULL,
+department varchar(20) NOT NULL,
+primary key(id)
+)
